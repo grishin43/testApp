1a8550da6394b03571d9bc388afc093ecf7054fa
Theodorshch <54853583+Theodorshch@users.noreply.github.com>
2019-12-02 17:41:59 +0200

    resolve auth routes if user is logged in

---
+++ src/app/_services/anonymous-guard.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnonymousGuardService implements CanActivate {
+
+  constructor(private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!localStorage.getItem('currentUser')) {
+      return true;
+    }
+    this.router.navigate(['/admin']);
+    return false;
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {RouteguardService} from './_services/routeguard.service';
+import {AnonymousGuardService} from './_services/anonymous-guard.service';
+
 
 const routes: Routes = [
   {path: '', component: LoginComponent, pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'login', canActivate: [AnonymousGuardService], component: LoginComponent},
+  {path: 'register', canActivate: [AnonymousGuardService], component: RegisterComponent},
   {path: 'admin', canActivate: [RouteguardService], loadChildren: () => import(`./admin/admin.module`).then(m => m.AdminModule)},
   {path: '**', redirectTo: ''}
 ];
